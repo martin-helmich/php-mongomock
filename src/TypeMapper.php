fec31730d08337781d7603e661ec3e6c21b71632
filecage <mail@dbeuchert.com>
2020-03-31 15:06:41 +0200

    Use separated TypeMapper

---
+++ src/TypeMapper.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Helmich\MongoMock;
+
+use MongoDB\Model\BSONArray;
+use MongoDB\Model\BSONDocument;
+
+class TypeMapper {
+
+    /** @const array */
+    const DEFAULT_TYPE_MAP = [
+        'array' => BSONArray::class,
+        'document' => BSONDocument::class,
+        'root' => BSONDocument::class
+    ];
+
+    /** @var array */
+    private $typeMap;
+
+    /**
+     * @param array $typeMapDefaultOverwrite
+     * @return TypeMapper
+     */
+    static function createWithDefault (array $typeMapDefaultOverwrite = []) : TypeMapper
+    {
+        return new static(array_merge(static::DEFAULT_TYPE_MAP, $typeMapDefaultOverwrite));
+    }
+
+    /**
+     * @param array $typeMap
+     */
+    function __construct (array $typeMap)
+    {
+        $this->typeMap = $typeMap;
+    }
+
+    /**
+     * @param BSONDocument $document
+     * @return BSONDocument|array
+     */
+    function map (BSONDocument $document) : iterable
+    {
+        /** @var BSONDocument $document */
+        $document = $this->typeMapRecursively($document);
+
+        if ($this->typeMap['document'] === 'array') {
+            $document = $document->getArrayCopy();
+        } elseif ($this->typeMap['document'] !== BSONDocument::class) {
+            $document = new $this->typeMap['document']($document->getArrayCopy());
+        }
+
+        return $document;
+    }
+
+    private function typeMapRecursively ($document)
+    {
+        foreach ($document as $key => &$value) {
+            if (is_array($value) && $this->typeMap['array'] !== 'array') {
+                $value = $this->typeMapRecursively($value);
+                $value = new $this->typeMap['array']($value);
+            }
+        }
+
+        return $document;
+    }
+
+    /**
+     * Merges a TypeMap with another TypeMap and returns a new mapper instance
+     *
+     * @param TypeMapper $typeMapper
+     * @return TypeMapper
+     */
+    function mergeWith(TypeMapper $typeMapper) : TypeMapper
+    {
+        $instance = clone $this;
+        $instance->typeMap = array_merge($instance->typeMap, $typeMapper->typeMap);
+
+        return $instance;
+    }
+
+}
